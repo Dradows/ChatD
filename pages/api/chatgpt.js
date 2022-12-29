bb29910f63666c34ca9b549cfc3f02f71e057600
Blaston <1023086065@qq.com>
2022-12-30 03:38:43 +0800

    update

---
+++ pages/api/chatgpt.js
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Configuration, OpenAIApi } from 'openai';
+import { NextRequest } from 'next/server';
+import { connectToDatabase } from '../../libs/mongodb';
+
+export default async function handler(req, res) {
+  const { database } = await connectToDatabase();
+  const collection = database.collection('chatD');
+  let ip = '';
+  if (req.headers['x-nf-client-connection-ip']) {
+    ip = req.headers['x-nf-client-connection-ip'];
+  } else if (req.headers['x-forwarded-for']) {
+    ip = req.headers['x-forwarded-for'].split(',')[0];
+  } else if (req.headers['x-real-ip']) {
+    ip = req.headers['x-real-ip'];
+  } else {
+    ip = req.connection.remoteAddress;
+  }
+  const { prompt } = req.body;
+  const configuration = new Configuration({
+    apiKey: process.env.apiKey,
+  });
+  const openai = new OpenAIApi(configuration);
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: prompt,
+    max_tokens: 2000,
+  });
+  const result = response.data.choices[0];
+  collection.insertOne({
+    ip: ip,
+    prompt: prompt,
+    result: result.text,
+  });
+  res.status(200).json(response.data);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,99 @@
 import { marked } from 'marked';
-import { Input, Form, Button, Spin } from 'antd';
-import { useState } from 'react';
+import { Input, Form, Button, Spin, Tabs } from 'antd';
+import { useState, useRef } from 'react';
 
 export default function Home() {
+  //add useRef
+  const [items, setItems] = useState([]);
   const [markdown, setMarkdown] = useState('');
   const [form] = Form.useForm();
   const { TextArea } = Input;
   const [loading, setLoading] = useState(false);
+  const [tab, setTab] = useState(0);
+  const index = useRef(0);
+  const last = useRef(0);
+  const prevToken = useRef(0);
+
   return (
     <>
       <div style={{ width: '60%', margin: 'auto' }}>
         <Form
-          layout='vertical'
-          style={{ paddingTop: '20px' }}
+          form={form}
+          style={{
+            paddingTop: '20px',
+          }}
           onFinish={async e => {
             setMarkdown('');
             setLoading(true);
-            const text = await fetch('/api/chatgpt/' + e.text);
+            let prompt = '';
+            for (let i = index.current; i < items.length; i++) {
+              prompt += items[i].label + items[i].text;
+            }
+            prompt += e.text;
+            const text = await fetch('/api/chatgpt', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                prompt: prompt,
+              }),
+            });
             const json = await text.json();
             console.log(json);
-            const result = json.text;
-            setMarkdown(marked.parse(result));
+            const result = json.choices[0].text;
+            const markdown = marked.parse(result);
+            setTab(items.length);
+            setItems([
+              ...items,
+              {
+                label: e.text,
+                children: (
+                  <div dangerouslySetInnerHTML={{ __html: markdown }} />
+                ),
+                key: items.length,
+                text: result,
+                token: json.usage.total_tokens - prevToken.current,
+              },
+            ]);
+            prevToken.current = json.usage.total_tokens;
+            while (prevToken.current > 2000) {
+              prevToken.current -= items[last.current].token;
+              last.current++;
+            }
+            form.setFieldValue('text', '');
             setLoading(false);
           }}
         >
-          <Form.Item name='text'>
-            <TextArea />
+          <Form.Item lable='text' name='text'>
+            <TextArea lable='text' name='text' />
           </Form.Item>
           <Form.Item>
-            <Button type='primary' htmlType='submit'>
+            <Button
+              type='primary'
+              htmlType='submit'
+              style={{ marginRight: '20px' }}
+            >
               Submit
+            </Button>
+            <Button
+              htmlType='button'
+              onClick={() => {
+                setTab(0);
+                setItems([]);
+                index.current = 0;
+                last.current = 0;
+                prevToken.current = 0;
+              }}
+            >
+              Reset
             </Button>
           </Form.Item>
         </Form>
-        {loading && <Spin />}
+        {/* {loading && <Spin />} */}
+        <Spin spinning={loading}>
+          <Tabs items={items} activeKey={tab} onChange={key => setTab(key)} />
+        </Spin>
         <div dangerouslySetInnerHTML={{ __html: markdown }} />
       </div>
     </>
