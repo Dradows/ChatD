b66cb3cbc3156dfdd50b092e1263c63be709edd9
Blaston <1023086065@qq.com>
2023-06-07 13:28:40 +0800

    m

--- pages/api/chatgpt.js
+++ pages/api/chatgpt.js
@@ -26,6 +26,6 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
   //add cors content-type
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'content-type');
   res.status(200).json(response.data);
 }
