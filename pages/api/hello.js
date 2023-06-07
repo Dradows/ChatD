eefcd902b56d8360d60cfe0ba2c9f4dbdd84da91
Blaston <1023086065@qq.com>
2023-06-07 13:02:06 +0800

    set cors

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,16 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export async function GET(request) {
-  return new Response('Hello, Next.js!', {
-    status: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-    },
-  });
-}
+export default function handler(req, res) {
+  // change res header,set cors
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
 
-// export default function handler(req, res) {
-//   res.status(200).json({ name: 'John Doe' })
-// }
+  res.status(200).json({ name: 'John Doe' });
+}
