f5392b2c38c81cb82247a67da14869f42ee22832
Blaston <1023086065@qq.com>
2023-06-07 12:34:48 +0800

    test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export async function GET(request) {
+  return new Response('Hello, Next.js!', {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  });
 }
+
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
